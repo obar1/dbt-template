2d753aad04eff70c1dd24bbcfb08f09ecc6d118a
obar1 <mamatucci@gmail.com>
2023-11-24 21:46:32 +0100

    ok model

---
+++ models/2model/iris_model.sql
@@ -0,0 +1,9 @@
+SELECT
+    class,
+    sepal_length,
+    sepal_width,
+    petal_length,
+    petal_width,
+    sepal_length * sepal_width AS sepal_area,
+    {{ dbt_utils.safe_divide('petal_length', 'petal_width') }} AS petal_ratio
+FROM {{ ref('iris_cleaned') }}

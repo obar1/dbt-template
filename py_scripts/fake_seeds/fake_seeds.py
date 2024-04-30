c45d974d2de4a67022b2d90f72da02d0b273fcfd
obar1 <mamatucci@gmail.com>
2024-04-30 20:31:29 +0200

    refactoring

--- py_scripts/fake_seeds/fake_seeds.py
+++ py_scripts/fake_seeds/fake_seeds.py
@@ -7,6 +7,10 @@ import pandas as  pd
 import csv 
 import exrex
 
+DUCKDB = 'duckdb'
+
+BIGQUERY = 'bigquery'
+
 CSV_SUFFFIX='.csv'
 DB_ID_COLUMN = 'db_id'
 SEED_METADATA_PREFIX = '$metadata_'
@@ -51,11 +55,6 @@ def make_seeds_from_bigquery(df):
         tables[ttt].append(cr) # add cr found
     return tables
 
-def make_seeds_from_duckdb(df):
-    print('make_seeds_from_duckdb')
-    # TODO:
-    return []
-
 def generate_seed_metadata(fn_seed, column_records: List[ColumnRecord]):
     """generate_seed_metadata
     TODO:
@@ -67,20 +66,34 @@ def generate_seed_metadata(fn_seed, column_records: List[ColumnRecord]):
 
 
 
-def get_rnd_data(cr: ColumnRecord, n_rows = N_ROWS) -> List[str]:
-    def _get_rnd_data_no_extra(cr:ColumnRecord):
-        if cr.column_type == "INT64":
-            return random.randint(0,100)
-        if cr.column_type == "NUMERIC":
-            return random.uniform(0,100)
-        if cr.column_type == "STRING":
-            return "".join(random.choice(string.ascii_letters) for _ in range(10))
+def get_rnd_data(cr: ColumnRecord, db_id, n_rows=N_ROWS) -> List[str]:
+    def _get_rnd_data_no_extra(cr: ColumnRecord, db_id):
+        if db_id==BIGQUERY:
+            if cr.column_type == "INT64":
+                return random.randint(0,100)
+            if cr.column_type == "NUMERIC":
+                return random.uniform(0,100)
+            if cr.column_type == "STRING":
+                return "".join(random.choice(string.ascii_letters) for _ in range(10))
+        elif db_id==DUCKDB:
+            if cr.column_type == "BIGINT":
+                return random.randint(0,100)
+            if cr.column_type == "DOUBLE":
+                return random.uniform(0,100)
+            if cr.column_type == "VARCHAR":
+                return "".join(random.choice(string.ascii_letters) for _ in range(10))
+        else:
+            ValueError(f'{db_id} not supported')
 
-    def  _get_rnd_data_with_extra(cr:ColumnRecord, sep = SEP_PIPE):
+    def  _get_rnd_data_with_extra(cr: ColumnRecord,  db_id,sep=SEP_PIPE):
         """supported 
         pattern
         list 
         range
+
+        Parameters
+        ----------
+        db_id
         """
         extra : str = cr.extra
         if  cr.extra != None:
@@ -93,24 +106,28 @@ def get_rnd_data(cr: ColumnRecord, n_rows = N_ROWS) -> List[str]:
             if "range=" in extra:
                 min, max= extra.split("range=")[1].split(sep)
                 return random.randint(int(min), int(max))
-        return _get_rnd_data_no_extra(cr)
+        return _get_rnd_data_no_extra(cr,db_id)
     res = []
     for i in range(0, n_rows):
         if cr.extra != None:
-            res.append(_get_rnd_data_with_extra(cr))
+            res.append(_get_rnd_data_with_extra(cr,db_id))
         else:
-            res.append(_get_rnd_data_no_extra(cr))
+            res.append(_get_rnd_data_no_extra(cr,db_id))
     return res
         
-def generate_seed_data(fn_seed, column_records: List[ColumnRecord]):
+def generate_seed_data(fn_seed, column_records: List[ColumnRecord], db_id):
     """generate_seed_data
     TODO:
+
+    Parameters
+    ----------
+    db_id
     """
     df = pd.DataFrame(column_records)
     fn_seed = SEED_DATA_PREFIX + fn_seed
     seed_data = {}
     for cr in column_records:
-        seed_data[cr.column_name] = get_rnd_data(cr)
+        seed_data[cr.column_name] = get_rnd_data(cr, db_id, N_ROWS)
     print(seed_data)
     ts = list(zip(*seed_data.values())) # trick to get mapped
     with open(fn_seed, 'w', newline='') as f:
@@ -121,7 +138,7 @@ def generate_seed_data(fn_seed, column_records: List[ColumnRecord]):
         
     
     
-def  generate_seeds(tables: dict):
+def  generate_seeds(tables: dict, db_id):
     
     def _get_fn_seed(k: str):
         return k.lower().replace(',','_').replace('-','_') + CSV_SUFFFIX
@@ -131,8 +148,8 @@ def  generate_seeds(tables: dict):
         fn_seed: str = _get_fn_seed(k)
         
         print(f"Working on {k}...")
-        generate_seed_metadata(fn_seed, column_records)  
-        generate_seed_data(fn_seed, column_records) 
+        generate_seed_metadata(fn_seed, column_records)
+        generate_seed_data(fn_seed, column_records,db_id)
 
 def check_metadata(args):
     df = pd.read_csv(args.fn_metadata)
@@ -144,10 +161,5 @@ def check_metadata(args):
 if __name__== "__main__":
     parsed = parse_input()
     db_id, df = check_metadata(parsed)
-    factories = {
-        'bigquery' : make_seeds_from_bigquery,
-        'duckdb' : make_seeds_from_duckdb
-    }
-    tables = factories[db_id](df)    
-    print(tables)
-    generate_seeds(tables)
+    tables = make_seeds_from_bigquery(df)
+    generate_seeds(tables,db_id)
